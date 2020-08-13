832ba50610c6bf711933ca4b10ce0c6e0c2e396d
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-08-13 20:58:13 +0800

    Update 股票买卖-动态规划.js

--- 股票买卖-动态规划.js
+++ 股票买卖-动态规划.js
@@ -78,3 +78,42 @@ var maxProfit = function(prices) {
 console.log(maxProfit([7,1,5,3,6,4]))
 console.log(maxProfit([7,6,4,3,1]))
 
+/*
+1. https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/
+k = 1
+ */
+var maxProfit1 = function(prices) {
+  const n = prices.length
+  const dp = new Array(n).fill(0).map(item => new Array(2).fill(0))
+  dp[0][0] = 0
+  dp[0][1] = -prices[1]
+
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i])
+    dp[i][1] = Math.max(dp[i-1][1], -prices[i])
+  }
+  return dp[n-1][0]
+}
+
+console.log(maxProfit1([7,1,5,3,6,4])) // 5
+
+/*
+2. https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+k = infinity
+ */
+var maxProfit2 = function(prices) {
+  const n = prices.length
+  const dp = new Array(n).fill(0).map(item => new Array(2).fill(0))
+  dp[0][0] = 0
+  dp[0][1] = -prices[1]
+
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i])
+    dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i])
+  }
+  return dp[n-1][0]
+}
+console.log(maxProfit2([7,1,5,3,6,4])) // 7
+
+/*第三题，k = +infinity with cooldown
+*/
