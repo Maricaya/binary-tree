afcc1eb1b40bcc434ba00b2cb1778b8a3898e6cc
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-10-23 13:58:19 +0800

    Update ajax.js

--- 实现类/ajax.js
+++ 实现类/ajax.js
@@ -6,54 +6,67 @@ ajax({
   timeout: 2000
 }).then((data) => {
   console.log(data)
-}).catch()
+})
 
 ajax({
   url: 'http://ux.lezhixing.com.cn/mock/385/region/schedule/role/add.do',
   method: 'POST',
-  data: {userId: 1,role: 1},
+  data: {userId: 1, role: 1},
   timeout: 2000
 }).then((data) => {
   console.log(data)
 }).catch()
-/**
- * `http`请求
- * @dec 适用`GET`和`POST`一样的拼接参数传参
- * @param {"GET"|"POST"} method 请求方法
- * @param {string} url 请求地址
- * @param {object} data 请求参数
- * @param {number} timeout 超时时间
- */
-function ajax({method, url, data, timeout }) {
-  const XHR = new XMLHttpRequest();
-  /** 请求参数 */
-  let sendData = "";
-  // 解析对象传参 &a=1&b=2&c=3
-  for (const key in data) {
-    sendData += "&" + key + "=" + data[key];
+
+// //请求数据
+ajax({
+  url: 'https://photo.sina.cn/aj/index',
+  method: 'JSONP',
+  data: {page: 1, cate: 'recommend'}
+}).then(data => {
+  console.log(data)
+})
+
+function handleData (data) {
+  let sendData = ''
+  for (const dataKey in data) {
+    sendData += `&${dataKey}=${data[dataKey]}`
   }
-  if (sendData) {
-    sendData = sendData.slice(1);
-  }
-  switch (method) {
-    case "GET":
-      url = `${url}?${sendData}`;
-      sendData = null;
-      break;
+  sendData = sendData.slice(1)
+  return sendData
+}
+
+function ajax({url, method, data, timeout, callback = 'callback'}) {
+  // a=123&b=456
+  if (method === 'JSONP') {
+    data.callback = callback
+    let sendData = handleData(data)
+    let script = document.createElement('script')
+    script.src = url + '?' + sendData
+    console.log(script) // https://photo.sina.cn/aj/index?page=1&cate=recommend&callback=jsoncallback
+    document.body.appendChild(script)
+    return new Promise((resolve, reject) => {
+      window[callback] = (value) => {
+        resolve(value)
+        //移除script元素
+        script.parentNode.removeChild(script)
+      }
+    })
   }
   return new Promise(((resolve, reject) => {
-    XHR.open(method, url, true);
-    XHR.timeout = timeout;
-    XHR.onreadystatechange = function () {
-      if (XHR.readyState === 4) {
-        if ((XHR.status >= 200 && XHR.status < 300) || XHR.status === 304) {
-          resolve(XHR.response);
-        } else {
-          reject(XHR);
-        }
+    let sendData = handleData(data)
+    if (method === 'GET') {
+      url += '?' + sendData
+      sendData = null
+    }
+    const xhr = new XMLHttpRequest()
+    xhr.open(method, url, true)
+    xhr.timeout = timeout
+    xhr.send(sendData);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) { resolve(xhr.response) } else { reject(xhr) }
       }
     }
-    XHR.send(sendData);
   }))
 }
 
